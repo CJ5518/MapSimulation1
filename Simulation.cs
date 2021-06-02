d4e6d2f081bc4553f865d568e87f36870c5dd239
Carson Rueber <carsonrueber@gmail.com>
2021-06-02 14:14:46 -0700

    Simulation normalcy

--- Simulation.cs
+++ Simulation.cs
@@ -11,190 +11,282 @@ using Unity.Jobs;
 using UnityEngine;
 
 public class Simulation {
-    public Texture2D[] textureArray;
-    public NativeArray<Simulation.TextureMetadata> textureMetadataArray;
-    public NativeArray<IntPtr> textureDataPointers;
-    public Color backgroundColor = new Color(0.2392157f, 0.6f, 0.7686275f);
-    public Texture2D drawTexture;
-    private const int batchCount = 13755;
-    public Simulation.SimulationDataStruct data = new Simulation.SimulationDataStruct() {
-        season = 0.0f,
-        seasonAdder = 0.0005f,
-        virusColor = Color.red,
-        healthyColor = Color.green,
-        propensityForVirusModifier = 0.0f
-    };
-    public NativeArray<Simulation.Cell> writeCells;
-    public NativeArray<Simulation.Cell> readCells;
+	//Array of input textures
+	public Texture2D[] textureArray;
+	//Metadata for the textures
+	public NativeArray<TextureMetadata> textureMetadataArray;
+	//Pointers to the texutreArray that are almost never valid
+	private NativeArray<IntPtr> textureDataPointers;
+	//The background color of the draw texture
+	public Color backgroundColor = new Color(0.2392157f, 0.6f, 0.7686275f);
+	//The draw texture
+	public Texture2D drawTexture;
+	//Batch count, messing with this will impact performance
+	//See unity parallel for jobs
+	private const int batchCount = 13755;
 
-    public Simulation(Texture2D[] textures) {
-        this.textureArray = textures;
-        this.Init();
-    }
+	//Cell struct, contains all the individual information for a cell
+	public struct Cell {
+		public float propensityForVirus; //How much we want virus
+		public float propensityToHeal; //how much we want to heal
+		public float health;
+		public bool inMask; //Are we in the mask
+	}
 
-    public unsafe void tickSimulation() {
-        for (int index = 0; index < this.textureArray.Length; ++index)
-            this.textureDataPointers[index] = new IntPtr(NativeArrayUnsafeUtility.GetUnsafeBufferPointerWithoutChecks<Color32>(this.textureArray[index].GetRawTextureData<Color32>()));
-        new Simulation.SimulationJob() {
-            readCells = this.readCells,
-            writeCells = this.writeCells,
-            drawTextureData = this.drawTexture.GetRawTextureData<Color32>(),
-            data = this.data,
-            textureDataPointers = this.textureDataPointers,
-            textureMetadataArray = this.textureMetadataArray
-        }.Schedule<Simulation.SimulationJob>(this.data.width * this.data.height, 13755).Complete();
-        this.drawTexture.Apply();
-        NativeArray<Simulation.Cell> readCells = this.readCells;
-        this.readCells = this.writeCells;
-        this.writeCells = readCells;
-        this.data.season += this.data.seasonAdder;
-        if ((double)this.data.season <= 1.0 && (double)this.data.season >= 0.0)
-            return;
-        this.data.seasonAdder *= -1f;
-    }
+	//Texture metadata struct
+	public struct TextureMetadata {
+		public float weight;
+	}
 
-    public void deleteNativeArrays() {
-        this.readCells.Dispose();
-        this.writeCells.Dispose();
-        this.textureDataPointers.Dispose();
-        this.textureMetadataArray.Dispose();
-    }
+	//Struct that holds all the data needed by the simulation
+	public struct SimulationDataStruct {
+		public int width; //Dimensions of input/output textures
+		public int height;
+		public float season; //Unused
+		public float seasonAdder;
+		public Color virusColor; //Colors for unhealthy/healthy cells
+		public Color healthyColor;
+		public float propensityForVirusModifier; //Added to propensity for virus
+	}
+	//Set the default values here
+	public SimulationDataStruct data = new SimulationDataStruct() {
+		season = 0.0f,
+		seasonAdder = 0.0005f,
+		virusColor = Color.red,
+		healthyColor = Color.green,
+		propensityForVirusModifier = 0.0f
+	};
 
-    public void Init() {
-        this.textureMetadataArray = new NativeArray<Simulation.TextureMetadata>(this.textureArray.Length, Allocator.Persistent);
-        this.textureDataPointers = new NativeArray<IntPtr>(this.textureArray.Length, Allocator.Persistent);
-        for (int index = 0; index < this.textureMetadataArray.Length; ++index) {
-            Simulation.TextureMetadata textureMetadata = this.textureMetadataArray[index];
-            textureMetadata.weight = 1f / (float)this.textureMetadataArray.Length;
-            this.textureMetadataArray[index] = textureMetadata;
-        }
-        this.data.width = this.textureArray[0].width;
-        this.data.height = this.textureArray[0].height;
-        for (int index = 0; index < this.textureArray.Length; ++index) {
-            if (this.textureArray[index].width != this.data.width || this.textureArray[index].height != this.data.height)
-                throw new Exception("Texture #" + index.ToString() + " is not of the same height and width as the other textures");
-            Texture2D texture2D = new Texture2D(this.data.width, this.data.height, TextureFormat.RGBA32, false);
-            texture2D.SetPixels(this.textureArray[index].GetPixels());
-            texture2D.Apply();
-            this.textureArray[index] = texture2D;
-        }
-        this.drawTexture = new Texture2D(this.data.width, this.data.height, TextureFormat.RGBA32, false);
-        this.drawTexture.filterMode = FilterMode.Point;
-        for (int x = 0; x < this.data.width; ++x) {
-            for (int y = 0; y < this.data.height; ++y)
-                this.drawTexture.SetPixel(x, y, this.backgroundColor);
-        }
-        this.InitCells();
-    }
+	//Cell buffers
+	//Internally: you always read from readCells, and write to writeCells
+	//Externally: writeCells is fairly useless, read/write to readCells if you want to read/change state
+	public NativeArray<Cell> writeCells;
+	public NativeArray<Cell> readCells;
 
-    public void InitCells() {
-        this.readCells = new NativeArray<Simulation.Cell>(this.data.width * this.data.height, Allocator.Persistent);
-        this.writeCells = new NativeArray<Simulation.Cell>(this.data.width * this.data.height, Allocator.Persistent);
-        for (int x = 0; x < this.data.width; ++x) {
-            for (int y = 0; y < this.data.height; ++y) {
-                int index1 = this.coordToIndex(x, y);
-                Simulation.Cell readCell = this.readCells[index1];
-                readCell.health = 1f;
-                readCell.inMask = true;
-                for (int index2 = 0; index2 < this.textureArray.Length; ++index2) {
-                    if ((double)this.textureArray[index2].GetPixel(x, y).a < 0.200000002980232) {
-                        readCell.inMask = false;
-                        break;
-                    }
-                }
-                this.readCells[index1] = readCell;
-                this.writeCells[index1] = readCell;
-            }
-        }
-    }
 
-    public int coordToIndex(int x, int y) => y * this.data.width + x;
+	//Constructor
+	//All textures must be of the same width and height, different formats are allowed
+	public Simulation(Texture2D[] textures) {
+		textureArray = textures;
+		Init();
+	}
 
-    public int coordToIndex(Vector2 vec) => (int)vec.y * this.data.width + (int)vec.x;
+	//The pun
+	//Ticks the simulation once
+	public unsafe void tickSimulation() {
+		//The unsafe part
+		//Get pointers to the raw texture data
+		for (int q = 0; q < textureArray.Length; q++) {
+			textureDataPointers[q] = new IntPtr(
+				NativeArrayUnsafeUtility.GetUnsafeBufferPointerWithoutChecks(textureArray[q].GetRawTextureData<Color32>())
+			);
+		}
+		//Create and start the simulation
+		var job = new SimulationJob() {
+			readCells = readCells,
+			writeCells = writeCells,
+			drawTextureData = this.drawTexture.GetRawTextureData<Color32>(),
+			data = this.data,
+			textureDataPointers = this.textureDataPointers,
+			textureMetadataArray = this.textureMetadataArray
+		};
 
-    public Vector2 indexToCoord(int index) {
-        Vector2 vector2;
-        vector2.x = (float)(index % this.data.width);
-        vector2.y = ((float)index - vector2.x) / (float)this.data.width;
-        return vector2;
-    }
+		JobHandle jobHandle = job.Schedule(data.width * data.height, 13755);
+		jobHandle.Complete();
 
-    public struct SimulationDataStruct {
-        public int width;
-        public int height;
-        public float season;
-        public float seasonAdder;
-        public Color virusColor;
-        public Color healthyColor;
-        public float propensityForVirusModifier;
-    }
+		drawTexture.Apply();
 
-    public struct TextureMetadata {
-        public float weight;
-    }
+		//Swap the cell buffers
+		NativeArray<Cell> tmp = readCells;
+		readCells = writeCells;
+		writeCells = tmp;
 
-    public struct Cell {
-        public float propensityForVirus;
-        public float propensityToHeal;
-        public float health;
-        public bool inMask;
-    }
+		//Updating the season variable
+		//Not currently used but it's here if we need it
+		data.season += data.seasonAdder;
+		if (data.season >= 1.0 || data.season <= 0.0)
+			data.seasonAdder *= -1f;
+	}
 
-    public struct SimulationJob : IJobParallelFor {
-        [ReadOnly]
-        public NativeArray<Simulation.Cell> readCells;
-        [WriteOnly]
-        public NativeArray<Simulation.Cell> writeCells;
-        [WriteOnly]
-        public NativeArray<Color32> drawTextureData;
-        [ReadOnly]
-        public Simulation.SimulationDataStruct data;
-        [ReadOnly]
-        public NativeArray<IntPtr> textureDataPointers;
-        [ReadOnly]
-        public NativeArray<Simulation.TextureMetadata> textureMetadataArray;
+	//Deletes the native arrays used by the simulation
+	//A required step that you can do in OnDestroy or whenever you're done with the simulation object
+	public void deleteNativeArrays() {
+		readCells.Dispose();
+		writeCells.Dispose();
+		textureDataPointers.Dispose();
+		textureMetadataArray.Dispose();
+	}
 
-        public unsafe void Execute(int index) {
-            if (!this.cellIsValid(index))
-                return;
-            Simulation.Cell readCell = this.readCells[index];
-            Simulation.Cell cell = readCell;
-            float num1 = 0.0f;
-            int num2 = 0;
-            int[] neighborIndices = this.getNeighborIndices(index);
-            for (int index1 = 0; index1 < neighborIndices.Length; ++index1) {
-                if (this.cellIsValid(neighborIndices[index1])) {
-                    ++num2;
-                    num1 += this.readCells[neighborIndices[index1]].health;
-                }
-            }
-            float num3 = num1 / (float)num2;
-            if ((double)num3 != 1.0) {
-                float num4 = Mathf.Clamp01(Mathf.Pow(readCell.propensityForVirus, 3f));
-                cell.health -= (float)(1.5 * (1.0 - (double)num3)) * num4;
-                cell.health = Mathf.Clamp01(cell.health);
-            }
-            float num5 = 0.0f;
-            for (int index1 = 0; index1 < this.textureDataPointers.Length; ++index1) {
-                Color32* textureDataPointer = (Color32*)(void*)this.textureDataPointers[index1];
-                num5 += (float)textureDataPointer[index].r / (float)byte.MaxValue * this.textureMetadataArray[index1].weight;
-            }
-            cell.propensityForVirus = Mathf.Clamp01(num5 + this.data.propensityForVirusModifier);
-            cell.propensityToHeal = 1f - cell.propensityForVirus;
-            Color color = Color.Lerp((double)readCell.health > 1.0 ? Color.Lerp(Color.white, this.data.healthyColor, readCell.health - 1f) : Color.Lerp(Color.white, this.data.virusColor, 1f - readCell.health), new Color(1f - readCell.propensityForVirus, 1f - readCell.propensityForVirus, 1f - readCell.propensityForVirus, 1f), 0.5f);
-            color.a = 1f;
-            this.drawTextureData[index] = (Color32)color;
-            this.writeCells[index] = cell;
-        }
+	//Init everything
+	public void Init() {
+		//Native arrays
+		textureMetadataArray = new NativeArray<TextureMetadata>(textureArray.Length, Allocator.Persistent);
+		textureDataPointers = new NativeArray<IntPtr>(textureArray.Length, Allocator.Persistent);
 
-        private bool cellIsValid(int index) => index >= 0 && index < this.data.width * this.data.height && this.readCells[index].inMask;
+		//Default initialization for textureMetadata
+		for (int q = 0; q < textureMetadataArray.Length; q++) {
+			TextureMetadata textureMetadata = textureMetadataArray[q];
+			textureMetadata.weight = 1.0f / textureMetadataArray.Length;
+			textureMetadataArray[q] = textureMetadata;
+		}
+		//We make the first one the example
+		data.width = textureArray[0].width;
+		data.height = textureArray[0].height;
 
-        public int[] getNeighborIndices(int index) => new int[4]
-        {
-      index - 1,
-      index + 1,
-      index + this.data.width,
-      index - this.data.width
-        };
-    }
+		//Converting the texture formats
+		for (int q = 0; q < textureArray.Length; q++) {
+			//Verify that it's the same size
+			if (textureArray[q].width != data.width || textureArray[q].height != data.height)
+				throw new Exception("Texture #" + q.ToString() + " is not of the same height and width as the other textures");
+
+			//Create dummy texture to copy the data in a new format
+			Texture2D dummy = new Texture2D(data.width, data.height, TextureFormat.RGBA32, false);
+			dummy.SetPixels(textureArray[q].GetPixels());
+			dummy.Apply();
+			textureArray[q] = dummy;
+		}
+		
+		//Create drawTexture
+		drawTexture = new Texture2D(data.width, data.height, TextureFormat.RGBA32, false);
+		drawTexture.filterMode = FilterMode.Point;
+
+		//Set all the pixels to be the background color
+		//When it gets updated, teh background ones won't be, so the background will remain constant
+		for (int x = 0; x < data.width; ++x) {
+			for (int y = 0; y < data.height; ++y) {
+				drawTexture.SetPixel(x, y, backgroundColor);
+			}
+		}
+		//Fire off a different init function
+		InitCells();
+	}
+
+	private void InitCells() {
+		//Native arrays
+		readCells = new NativeArray<Cell>(data.width * data.height, Allocator.Persistent);
+		writeCells = new NativeArray<Cell>(data.width * data.height, Allocator.Persistent);
+
+		//For every cell
+		for (int x = 0; x < data.width; x++) {
+			for (int y = 0; y < data.height; y++) {
+				int index = coordToIndex(x, y);
+
+				//Set some default cell values
+				Cell readCell = readCells[index];
+				readCell.health = 1f;
+
+				//If it's in the mask, and therefore should be updated
+				readCell.inMask = true;
+				for (int q = 0; q < textureArray.Length; q++) {
+					if (textureArray[q].GetPixel(x, y).a < 0.2f) {
+						readCell.inMask = false;
+						break;
+					}
+				}
+				
+				//Write back the cell
+				readCells[index] = readCell;
+				writeCells[index] = readCell;
+			}
+		}
+	}
+
+
+	//The real meat and potatoes
+	//Handles the simulation
+	public struct SimulationJob : IJobParallelFor {
+		//Inputs and outputs
+		[ReadOnly]
+		public NativeArray<Cell> readCells;
+		[WriteOnly]
+		public NativeArray<Cell> writeCells;
+		[WriteOnly]
+		public NativeArray<Color32> drawTextureData;
+		[ReadOnly]
+		public SimulationDataStruct data;
+		[ReadOnly]
+		public NativeArray<IntPtr> textureDataPointers;
+		[ReadOnly]
+		public NativeArray<TextureMetadata> textureMetadataArray;
+
+		//The function that gets called for every index
+		public unsafe void Execute(int index) {
+			if (!cellIsValid(index))
+				return;
+			Cell readCell = readCells[index];
+			Cell writeCell = readCell;
+
+			//Color32* textureDataPointer = (Color32*)textureDataPointers[texIdx];
+			//Then just use it as an array
+
+
+			//Get a healthyness percentage
+			float sumHealth = 0.0f;
+			int numNeighbors = 0;
+			int[] neighborIndices = getNeighborIndices(index);
+			for (int q = 0; q < neighborIndices.Length; q++) {
+				if (cellIsValid(neighborIndices[q])) {
+					numNeighbors++;
+					sumHealth += readCells[neighborIndices[q]].health;
+				}
+			}
+			float healthyPercentage = sumHealth / (float)numNeighbors;
+
+			//Damage yourself
+			if (healthyPercentage < 1.0f) {
+				float factor = Mathf.Clamp01(Mathf.Pow(readCell.propensityForVirus, 3f));
+				writeCell.health -= (1.5f * (1.0f - healthyPercentage)) * factor;
+				writeCell.health = Mathf.Clamp01(writeCell.health);
+			}
+
+			//Calculate the propensity for virus
+			float propensityForVirus = 0.0f;
+			for (int q = 0; q < textureDataPointers.Length; q++) {
+				Color32* textureDataPointer = (Color32*)textureDataPointers[q];
+				propensityForVirus += (textureDataPointer[index].r / (float)byte.MaxValue) * textureMetadataArray[q].weight;
+			}
+			//Set the values
+			writeCell.propensityForVirus = Mathf.Clamp01(propensityForVirus + this.data.propensityForVirusModifier);
+			writeCell.propensityToHeal = 1f - writeCell.propensityForVirus;
+
+			//Compute the color
+			Color color = Color.Lerp(Color.white, data.virusColor, 1.0f - writeCell.health);
+			color.a = 1f;
+
+			//Write back the data
+			drawTextureData[index] = color;
+			writeCells[index] = writeCell;
+		}
+		//Cell is in bounds and also in the mask
+		private bool cellIsValid(int index) {
+			return index >= 0 && index < (data.width * data.height) && readCells[index].inMask;
+		}
+
+		//Returns the indices of the neighbors to an index
+		//Indices may or may not be valid, or even in the array at all
+		public int[] getNeighborIndices(int index) {
+			int[] ret = new int[4];
+			ret[0] = index - 1;
+			ret[1] = index + 1;
+			ret[2] = index + data.width;
+			ret[3] = index - data.width;
+			return ret;
+		}
+	}
+
+	//Coordinate system functions
+	public int coordToIndex(int x, int y) {
+		return (y * data.width) + x;
+	}
+
+	public int coordToIndex(Vector2 vec) {
+		return ((int)vec.y * data.width) + (int)vec.x;
+	}
+
+	public Vector2 indexToCoord(int index) {
+		Vector2 ret;
+		ret.x = index % data.width;
+		ret.y = (index - ret.x) / data.width;
+		return ret;
+	}
 }
