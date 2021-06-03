c66597fbb12a841ad34051bc63a7ca8d72a5d4b7
Carson Rueber <carsonrueber@gmail.com>
2021-06-03 16:53:10 -0700

    Add functions to go in the opposite direction

--- Projection.cs
+++ Projection.cs
@@ -5,9 +5,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-//Projects lat/longs into screen coordinates
+//Projects lat/longs into screen coordinates and back again
 //https://stackoverflow.com/a/4565555
 public class Projection {
+
+	//Single number lat/longs
 	public static float projectLongitude(float x) {
 		return (Screen.width / 360.0f) * (180.0f + x);
 	}
@@ -21,6 +23,7 @@ public class Projection {
 		return (Screen.height / 180.0) * (90.0 - y);
 	}
 
+	//Vector lat/longs
 	public static Vector2 projectVector(Vector2 vector) {
 		Vector2 ret;
 		ret.x = projectLongitude(vector.x);
@@ -31,6 +34,35 @@ public class Projection {
 		Vector2Double ret;
 		ret.x = projectLongitude(vector.x);
 		ret.y = projectLatitude(vector.y);
+		return ret;
+	}
+
+
+	//Single number projections
+	public static float projectionToLongitude(float x) {
+		return (x / (Screen.width / 360.0f)) - 180.0f;
+	}
+	public static double projectionToLongitude(double x) {
+		return (x / (Screen.width / 360.0)) - 180.0;
+	}
+	public static float projectionToLatitude(float y) {
+		return 90.0f - (y / (Screen.height / 180.0f));
+	}
+	public static double projectionToLatitude(double y) {
+		return 90.0 - (y / (Screen.height / 180.0));
+	}
+
+	//Vector projections
+	public static Vector2 projectionToLatLongs(Vector2 vector) {
+		Vector2 ret;
+		ret.x = projectionToLongitude(vector.x);
+		ret.y = projectionToLatitude(vector.y);
+		return ret;
+	}
+	public static Vector2Double projectionToLatLongs(Vector2Double vector) {
+		Vector2Double ret;
+		ret.x = projectionToLongitude(vector.x);
+		ret.y = projectionToLatitude(vector.y);
 		return ret;
 	}
 }
