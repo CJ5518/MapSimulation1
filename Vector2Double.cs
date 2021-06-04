a8de70637a569d9e4ee37c28186900a10db55893
Carson Rueber <carsonrueber@gmail.com>
2021-06-04 09:53:31 -0700

    Add a constructor

--- Vector2Double.cs
+++ Vector2Double.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//By Carson Rueber
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,4 +9,9 @@ using UnityEngine;
 
 public struct Vector2Double {
 	public double x, y;
+
+	public Vector2Double(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
 }
