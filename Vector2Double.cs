840246752428a2f2c05b784ff6e4f3a767aa5570
Carson Rueber <carsonrueber@gmail.com>
2021-06-04 10:00:50 -0700

    Add some operator overloads

--- Vector2Double.cs
+++ Vector2Double.cs
@@ -9,9 +9,25 @@ using UnityEngine;
 
 public struct Vector2Double {
 	public double x, y;
-
+	
+	//Constructor
 	public Vector2Double(double x, double y) {
 		this.x = x;
 		this.y = y;
+	}
+
+	//Operators
+
+	//Multiplication
+	public static Vector2Double operator *(Vector2Double self, double a) {
+		return new Vector2Double(self.x * a, self.y * a);
+	}
+	public static Vector2Double operator *(Vector2Double self, float a) {
+		return new Vector2Double(self.x * a, self.y * a);
+	}
+
+	//Conversions
+	public static implicit operator Vector2Double(Vector2 vec) {
+		return new Vector2Double(vec.x, vec.y);
 	}
 }
