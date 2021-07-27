0284685ecf25ef93dda450b91b46454d65a7f382
Carson Rueber <carsonrueber@gmail.com>
2021-07-27 14:49:16 -0700

    Collab

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//put this script on the camera
+public class CameraControl : MonoBehaviour {
+
+
+
+
+    void Start() {
+        
+    }
+    void Update() {
+        
+    }
+}
