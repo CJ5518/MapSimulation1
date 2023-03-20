8a93ea405ce8bcd719ec282c6304def156b2fe74
CarsonRueber <carsonrueber@gmail.com>
2023-03-20 15:06:39 -0700

    Fix texture swapping

--- Assets/Scripts/SwapMapBaseTexture.cs
+++ Assets/Scripts/SwapMapBaseTexture.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public class SwapMapBaseTexture : MonoBehaviour {
 
-    public Texture sateliteTex;
-    public Texture populationTex;
-    public Texture highwayTex;
-    
-    public Renderer mesh;
+	public Texture sateliteTex;
+	public Texture populationTex;
+	public Texture highwayTex;
+	
+	public Renderer mesh;
 
-    public void ChangeMaterial(int index){
-        if(index < 1){
-            mesh.material.SetTexture("_BaseTex", sateliteTex);
-        }
-        if(index > 1){
-            mesh.material.SetTexture("_BaseTex", highwayTex);
-        }
-        else {
-            mesh.material.SetTexture("_BaseTex", populationTex);
-        }
-    }
+	public void ChangeMaterial(int index){
+		Debug.Log(index);
+		if(index == 0){
+			mesh.material.SetTexture("_BaseTex", sateliteTex);
+		}
+		if (index == 1) {
+			mesh.material.SetTexture("_BaseTex", populationTex);
+		}
+		if(index == 2){
+			mesh.material.SetTexture("_BaseTex", highwayTex);
+		}
+	}
 }
